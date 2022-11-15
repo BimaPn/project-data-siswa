67435786d13b2225641ee81e4823ab6af0301fb0
unknown <bimaptr12@gmail.com>
2022-11-15 19:48:38 +0700

    seventh commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,18 +18,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        User::create([
-            'name'=>'Bima Putra',
-            'username'=>'bimapn',
-            'email'=>'bimaptr@gmail.com',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-        ]);
 
         Siswa::factory(50)->create();
 
